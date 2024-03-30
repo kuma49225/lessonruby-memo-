a495241a1977504f38403c65714d36a80cb3a7d6
熊谷翔太 <syota@kumagaiutanoAir.lan>
2024-03-30 17:02:13 +0900

    second reviw

--- memo.rb
+++ memo.rb
@@ -1,13 +1,12 @@
 require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
-
+loop do
     puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
-
     memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
 
     # if文を使用して続きを作成していきましょう。
     # 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
 
-if memo_type == 1
+ if memo_type == 1
 
     puts "拡張子を除いたファイル名を入力してください"
 
@@ -23,11 +22,13 @@ if memo_type == 1
     CSV.open("#{file_name}.csv", "w") do |csv|
     memos.each {|memo|csv << [memo]}
     end
+    break
 
-elsif memo_type == 2
+ elsif memo_type == 2
     puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"
     file_name = gets.chomp.strip
     puts "現在のメモ内容"
+   begin
     existing_memos = CSV.read("#{file_name}.csv") 
     existing_memos.each_with_index do |memo, index| 
     puts "#{memo.join(', ')}" 
@@ -42,10 +43,14 @@ elsif memo_type == 2
     end
     CSV.open("#{file_name}.csv","w")do|csv|
     new_memos.each {|memo|csv << [memo]}
-end
-
-else
-puts "入力値が不正です"
+    end
+    break
+   rescue Errno::ENOENT
+    puts "指定のファイルは存在しません"
+   end
+ else
+ puts "入力値が不正です"
+ end
 end
 
 
